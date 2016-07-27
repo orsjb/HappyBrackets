005f99cbe80c508f01b39ad63772c2183cd575a3
Ollie Bown <ollie@icarus.nu>
2016-07-26 22:31:33 -0700

    Added BasicBroadcastTest to tutorial.

---
+++ Distribution/HappyBrackets Developer Kit/HappyBrackets Project/src/net/happybrackets/tutorial/session7/BasicBroadcastTest.java
@@ -0,0 +1,34 @@
+package net.happybrackets.tutorial.session7;
+
+import de.sciss.net.OSCListener;
+import de.sciss.net.OSCMessage;
+import net.beadsproject.beads.core.Bead;
+import net.happybrackets.core.HBAction;
+import net.happybrackets.device.HB;
+
+import java.net.SocketAddress;
+
+/**
+ * Created by ollie on 26/07/2016.
+ */
+public class BasicBroadcastTest implements HBAction {
+
+    @Override
+    public void action(HB hb) {
+        hb.addBroadcastListener(new OSCListener() {
+            @Override
+            public void messageReceived(OSCMessage oscMessage, SocketAddress socketAddress, long l) {
+                System.out.println("Message received: " + oscMessage.getName());
+            }
+        });
+        hb.pattern(new Bead() {
+            @Override
+            protected void messageReceived(Bead bead) {
+                if(hb.clock.getCount() % 32 == 0) {
+                    hb.broadcast.broadcast("/test" + hb.myIndex());
+                }
+            }
+        });
+    }
+
+}
