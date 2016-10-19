111e0da4481a8f2e3613a563ed1b56023086dbd9
Ollie Bown <ollie@icarus.nu>
2016-10-19 15:55:23 +1100

    Created the SimpleCheckable and example code.

---
+++ HappyBrackets/src/main/java/net/happybrackets/extras/assignment_autograding/SimpleCheckable.java
@@ -0,0 +1,8 @@
+package net.happybrackets.extras.assignment_autograding;
+
+/**
+ * Created by ollie on 19/10/16.
+ */
+public interface SimpleCheckable {
+    public void task(Object... args);
+}
---
+++ HappyBrackets/src/test/java/net/happybrackets/extras/assignment_autograding/ExampleSimpleCheckable.java
@@ -0,0 +1,7 @@
+package net.happybrackets.extras.assignment_autograding;
+
+/**
+ * Created by ollie on 19/10/16.
+ */
+public class ExampleSimpleCheckable {
+}
