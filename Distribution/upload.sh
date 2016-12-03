2b1eae5c2d5930d4c4f8716a45b12325ab84dd3b
Ollie Bown <ollie@icarus.nu>
2016-12-03 19:01:49 +1100

    Clean up of .gitignore.

--- Distribution/upload.sh
+++ Distribution/upload.sh
@@ -6,3 +6,4 @@ zip -r ../../HappyBracketsDeviceRuntime.zip HappyBrackets/
 cd ../../
 scp *.zip happybrackets@wayne.dreamhost.com:happybrackets.net/downloads/
 rm *.zip
+scp -r HappyBrackets\ Developer\ Kit/Device/HappyBrackets\ Project/libs/docs/hb/javadoc happybrackets@wayne.dreamhost.com:happybrackets.net/doc
