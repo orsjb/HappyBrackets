838cf2ce80397b7ca9f2b06bd1c5c0ee2da87cc8
Ollie Bown <ollie@icarus.nu>
2017-01-16 11:19:56 +1100

    Added setup of ssh login to the device setup script. Rebuilt project. Fixed some javadoc bugs.

--- Distribution/upload.sh
+++ Distribution/upload.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Builds the deployment zips and uploads them.
+# Before you run this you will need to run ‘gradle deploy’ from within the main HappyBrackets dev project, and also rebuilt the IntelliJ plugin and move it into the distribution folder (or re-run ‘gradle deploy’ which will do that).
+
 # TODO: remove .DS_Store and other unwanted files.
 
 zip -r HappyBracketsDeveloperKit.zip HappyBrackets\ Developer\ Kit/
