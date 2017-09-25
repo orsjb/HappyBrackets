7997deade80a80b6671648cb7cd5585bae16e0e3
angelo fraietta <angelo_f@smartcontroller.com.au>
2017-09-26 08:24:37 +1000

    Removed previous defines defined in SH but not required for bat file

--- Distribution/HappyBrackets Developer Kit/Device/HappyBrackets/scripts/run-local.bat
+++ Distribution/HappyBrackets Developer Kit/Device/HappyBrackets/scripts/run-local.bat
@@ -1,14 +1,5 @@
 cd ..\
 
-BUF=1024
-SR=44100
-BITS=16
-INS=0
-OUTS=1 
-AUTOSTART=true 
-ACCESSMODE=local
-ACTION=
-
 echo "Running HappyBrackets"
 
 java -jar HB.jar net.happybrackets.device.DeviceMain buf=1024 sr=44100 bits=16 ins=0 outs=1 start=true access=local
