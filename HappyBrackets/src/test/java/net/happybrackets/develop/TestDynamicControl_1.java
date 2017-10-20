070bf58bc3670ebc537739066693c106064dd907
angelo fraietta <angelo_f@smartcontroller.com.au>
2017-10-20 12:03:16 +1100

    added TestDynamicControl_1.java

---
+++ HappyBrackets/src/test/java/net/happybrackets/develop/TestDynamicControl_1.java
@@ -0,0 +1,54 @@
+package net.happybrackets.develop;
+
+import net.beadsproject.beads.core.Bead;
+import net.beadsproject.beads.data.Buffer;
+import net.beadsproject.beads.data.Pitch;
+import net.beadsproject.beads.events.KillTrigger;
+import net.beadsproject.beads.ugens.Envelope;
+import net.beadsproject.beads.ugens.Gain;
+import net.beadsproject.beads.ugens.Noise;
+import net.beadsproject.beads.ugens.WavePlayer;
+import net.happybrackets.core.HBAction;
+import net.happybrackets.device.HB;
+
+public class TestDynamicControl_1 implements HBAction {
+    @Override
+    public void action(HB hb) {
+        hb.masterGainEnv.setValue(0.2f);
+        hb.clock.getIntervalUGen().setValue(2000);
+        hb.clock.clearMessageListeners();
+        hb.pattern(new Bead() {
+            @Override
+            protected void messageReceived(Bead bead) {
+                if(hb.clock.getCount() % 8 == 0) {
+                    float freq = Pitch.forceFrequencyToScale(hb.rng.nextFloat() * 2000 + 100, Pitch.dorian);
+                    WavePlayer wp = new WavePlayer(hb.ac, freq, Buffer.SINE);
+                    Envelope e = new Envelope(hb.ac, hb.rng.nextFloat() * 0.03f + 0.1f);
+                    Gain g = new Gain(hb.ac, 1, e);
+                    e.addSegment(0, 5000, new KillTrigger(g));
+                    g.addInput(wp);
+                    hb.sound(g);
+                }
+                if(hb.clock.getCount() % 6 == 5) {
+                    float freq = Pitch.forceFrequencyToScale(hb.rng.nextFloat() * 1000 + 100, Pitch.dorian);
+                    WavePlayer wp = new WavePlayer(hb.ac, freq, Buffer.SINE);
+                    Envelope e = new Envelope(hb.ac, 0);
+                    Gain g = new Gain(hb.ac, 1, e);
+                    e.addSegment(hb.rng.nextFloat() * 0.02f + 0.04f, 1000);
+                    e.addSegment(0, 100, new KillTrigger(g));
+                    g.addInput(wp);
+                    hb.sound(g);
+                }
+                if(hb.clock.getCount() % 4 == 0) {
+                    Noise n = new Noise(hb.ac);
+                    Envelope e = new Envelope(hb.ac, hb.rng.nextFloat() * 0.01f + 0.02f);
+                    Gain g = new Gain(hb.ac, 1, e);
+                    e.addSegment(0, 5, new KillTrigger(g));
+                    g.addInput(n);
+                    hb.sound(g);
+                }
+            }
+        });
+    }
+
+}
