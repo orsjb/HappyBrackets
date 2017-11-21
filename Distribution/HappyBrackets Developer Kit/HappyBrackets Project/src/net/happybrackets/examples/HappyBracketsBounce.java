78c05b55cdbba53f02f1fa3ee4dd8937fca465b7
angelo fraietta <angelo_f@smartcontroller.com.au>
2017-11-21 13:25:24 +1100

    Added Acelerometer

--- Distribution/HappyBrackets Developer Kit/HappyBrackets Project/src/net/happybrackets/examples/HappyBracketsBounce.java
+++ Distribution/HappyBrackets Developer Kit/HappyBrackets Project/src/net/happybrackets/examples/HappyBracketsBounce.java
@@ -9,6 +9,8 @@ import net.happybrackets.core.control.ControlScope;
 import net.happybrackets.core.control.ControlType;
 import net.happybrackets.core.control.DynamicControl;
 import net.happybrackets.device.HB;
+import net.happybrackets.device.sensors.LSM9DS1;
+import net.happybrackets.device.sensors.SensorUpdateListener;
 
 /**
  * The purpose of this Sketch is to develop the appropriate Sketch for running a bouncing Sound On PI
@@ -16,7 +18,7 @@ import net.happybrackets.device.HB;
  * Y Axis will change the speed
  * Z Axis will change modulation of Pitch
  */
-public class HappybracketsDevelopBounce implements HBAction{
+public class HappyBracketsBounce implements HBAction {
     float initialFreq = 500;
 
     Clock clock;
@@ -95,27 +97,27 @@ public class HappybracketsDevelopBounce implements HBAction{
 
         // We said X would be Pitch
         hb.createDynamicControl(this, ControlType.FLOAT, CONTROL_PREFIX + "x", 0, -1, 1)
-            .addControlListener(new DynamicControl.DynamicControlListener() {
-                @Override
-                public void update(DynamicControl dynamicControl) {
-                    float val = (float) dynamicControl.getValue();
-                    float base_freq = (float) Math.pow(100, val + 1) + 50; // this will give us values from 50 to 10050
-                    freq_control.setValue((base_freq));
-                }
-            });
+                .addControlListener(new DynamicControl.DynamicControlListener() {
+                    @Override
+                    public void update(DynamicControl dynamicControl) {
+                        float val = (float) dynamicControl.getValue();
+                        float base_freq = (float) Math.pow(100, val + 1) + 50; // this will give us values from 50 to 10050
+                        freq_control.setValue((base_freq));
+                    }
+                });
 
         // Y will control the speed
         hb.createDynamicControl(this, ControlType.FLOAT, CONTROL_PREFIX + "y", 0, -1, 1)
-            .addControlListener(new DynamicControl.DynamicControlListener() {
-                @Override
-                public void update(DynamicControl dynamicControl) {
-                    float val = (float) dynamicControl.getValue();
-                    // we want to make it an int ranging from 8 to 512
-                    val += 2; // Now it is 1 t0 3
-                    float speed = (float) Math.pow(2, val * 3);
-                    speed_control.setValue(speed);
-                }
-            });
+                .addControlListener(new DynamicControl.DynamicControlListener() {
+                    @Override
+                    public void update(DynamicControl dynamicControl) {
+                        float val = (float) dynamicControl.getValue();
+                        // we want to make it an int ranging from 8 to 512
+                        val += 2; // Now it is 1 t0 3
+                        float speed = (float) Math.pow(2, val * 3);
+                        speed_control.setValue(speed);
+                    }
+                });
 
         // Z axis will Change mod speed and depth
         hb.createDynamicControl(this, ControlType.FLOAT, CONTROL_PREFIX + "z", 0, -1, 1)
@@ -128,10 +130,8 @@ public class HappybracketsDevelopBounce implements HBAction{
                         float abs_val = Math.abs(val);
                         float depth_freq = abs_val * 5000;
                         float mod_freq = abs_val * 10;
-
-                        range_control.setValue(mod_freq);
-                        depth_control.setValue(depth_freq);
-
+                        modDepth.setValue(depth_freq);
+                        modFreq.setValue(mod_freq);
                     }
                 });
 
@@ -139,9 +139,41 @@ public class HappybracketsDevelopBounce implements HBAction{
 
         hb.ac.out.addDependent(clock);
 
+        LSM9DS1 mySensor = (LSM9DS1) hb.getSensor(LSM9DS1.class);
+        mySensor.addListener(new SensorUpdateListener() {
+             @Override
+             public void sensorUpdated() {
+                 double zAxis = mySensor.getAccelerometerData()[2];
+                 double yAxis = mySensor.getAccelerometerData()[1];
+                 double xAxis = mySensor.getAccelerometerData()[0];
 
-        //this is the FM synth
-        WavePlayer modulator = new WavePlayer(hb.ac, modFreq, Buffer.SINE);
+                 // X was Freq
+                 float val = (float) xAxis;
+                 float base_freq = (float) Math.pow(100, val + 1) + 50; // this will give us values from 50 to 10050
+                 baseFreq.setValue(base_freq);
+
+                 // Y was Speed
+                 val = (float) yAxis;
+                 // we want to make it an int ranging from 8 to 512
+                 val += 2; // Now it is 1 t0 3
+                 float speed = (float) Math.pow(2, val * 3);
+                 clock.setTicksPerBeat((int) speed);
+
+                 // Z was Modulation
+                 val = (float) zAxis;
+                 // anything off zero will give us a value
+                 //Ranging from 0 to 1
+                 float abs_val = Math.abs(val);
+                 float depth_freq = abs_val * 5000;
+                 float mod_freq = abs_val * 10;
+                 modDepth.setValue(depth_freq);
+                 modFreq.setValue(mod_freq);
+             }
+         });
+
+
+                //this is the FM synth
+                WavePlayer modulator = new WavePlayer(hb.ac, modFreq, Buffer.SINE);
         Function modFunction = new Function(modulator, modDepth, baseFreq) {
             @Override
             public float calculate() {
