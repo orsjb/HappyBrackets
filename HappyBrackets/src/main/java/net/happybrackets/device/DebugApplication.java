7b794f25ba184e938a77a01e9a74d4633477e13d
angelofraietta <angelo_f@smartcontroller.com.au>
2017-12-03 14:15:13 +1100

    Added Debug Aplication so we can see Dynamic Controls

---
+++ HappyBrackets/src/main/java/net/happybrackets/device/DebugApplication.java
@@ -0,0 +1,55 @@
+package net.happybrackets.device;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import net.happybrackets.controller.gui.DynamicControlScreen;
+import net.happybrackets.core.control.ControlMap;
+import net.happybrackets.core.control.DynamicControl;
+
+import java.util.List;
+
+public class DebugApplication extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        System.out.println("THis is DebugApplication");
+
+        DynamicControlScreen debugControlsScreen = new DynamicControlScreen("Debug");
+
+
+        debugControlsScreen.addDynamicControlScreenLoadedListener(new DynamicControlScreen.DynamicControlScreenLoaded() {
+            @Override
+            public void loadComplete(DynamicControlScreen screen, boolean loaded) {
+                // screen load is complete.
+                //Now Add all controls
+                ControlMap control_map = ControlMap.getInstance();
+
+                List<DynamicControl> controls = control_map.GetSortedControls();
+
+                for (DynamicControl control : controls) {
+                    if (control != null) {
+                        debugControlsScreen.addDynamicControl(control);
+                        debugControlsScreen.show();
+                    }
+                }
+
+                // Now make a listener for Dynamic Controls that are made during the HB Action event
+                control_map.addDynamicControlCreatedListener(new ControlMap.dynamicControlCreatedListener() {
+                    @Override
+                    public void controlCreated(DynamicControl control) {
+                        debugControlsScreen.addDynamicControl(control);
+                        debugControlsScreen.show();
+                    }
+                });
+
+            }
+        });
+
+        // now we have a listener to see when stage is made, let us load the stage
+        debugControlsScreen.createDynamicControlStage();
+    }
+}
