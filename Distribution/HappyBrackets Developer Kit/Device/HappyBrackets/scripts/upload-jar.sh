d710d5c6c3919419b5098c9b78fb2f1752edf5bd
Angelo Fraietta <angelo_f@smartcontroller.com.au>
2017-12-11 14:39:49 +1100

    Added Upload and Download Audio scripts

--- Distribution/HappyBrackets Developer Kit/Device/HappyBrackets/scripts/upload-jar.sh
+++ Distribution/HappyBrackets Developer Kit/Device/HappyBrackets/scripts/upload-jar.sh
@@ -26,13 +26,11 @@ if [ "$DEVICE_NAME" != "" ]; then
     scp HB.jar $HOST_ADDRESS:~/HappyBrackets
 
 # now we need to SSH into deviceso we can do a reboot
-    echo "We need to reboot your device. The following line will be sent when you enter you password"
+    echo "We need to reboot your device to prevent disk becoming corrupt. The following line will be sent when you enter you password"
 
     echo "sudo shutdown -r now"
 
     ssh $HOST_ADDRESS "sudo shutdown -r now"
-
-
 
 else
     echo "You need to enter the device name as argument to call. eg ${0} hb-001d43801b7a.local"
