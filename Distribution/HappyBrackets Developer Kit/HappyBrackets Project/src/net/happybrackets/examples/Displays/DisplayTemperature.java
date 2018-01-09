9c4cf6c3b8e42526c2d17650a9e4924a98ebea90
angelo fraietta <angelo_f@smartcontroller.com.au>
2018-01-10 09:07:32 +1100

    Added accelerometer

--- Distribution/HappyBrackets Developer Kit/HappyBrackets Project/src/net/happybrackets/examples/Displays/DisplayTemperature.java
+++ Distribution/HappyBrackets Developer Kit/HappyBrackets Project/src/net/happybrackets/examples/Displays/DisplayTemperature.java
@@ -5,7 +5,7 @@ import net.happybrackets.core.control.ControlType;
 import net.happybrackets.core.control.DynamicControl;
 import net.happybrackets.device.HB;
 import net.happybrackets.device.sensors.Accelerometer;
-import net.happybrackets.device.sensors.LSM9DS1;
+import net.happybrackets.device.sensors.HTS221;
 import net.happybrackets.device.sensors.SensorUpdateListener;
 
 import java.lang.invoke.MethodHandles;
@@ -13,9 +13,7 @@ import java.lang.invoke.MethodHandles;
 /**
  * Display the values of the sensor in the GUI
  */
-public class DebugAccelerometer implements HBAction {
-
-    boolean initialisedMaxMin = false;
+public class DisplayTemperature implements HBAction {
 
     public static void main(String[] args) {
 
@@ -29,18 +27,21 @@ public class DebugAccelerometer implements HBAction {
     @Override
     public void action(HB hb) {
 
-        Accelerometer mySensor = (Accelerometer)hb.getSensor(Accelerometer.class);
+
+        HTS221 mySensor = (HTS221) hb.getSensor(HTS221.class);
+
+        DynamicControl control_temp = hb.createDynamicControl(ControlType.FLOAT, "Temperature");
 
         if (mySensor != null) {
-            mySensor.setRounding(5);
-
             mySensor.addListener(new SensorUpdateListener() {
+
+
                 @Override
                 public void sensorUpdated() {
-                    System.out.println(mySensor.getAccelerometerX());
-                    System.out.println(mySensor.getAccelerometerY());
-                    System.out.println(mySensor.getAccelerometerZ());
+
+                    control_temp.setValue(mySensor.getTemperatureData());
                 }
+
             });
         }
     }
