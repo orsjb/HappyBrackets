306923cc69c863aad4e1323c2854fce0a1bbe534
Corrected Type <angelofraietta@Angelos-MacBook-Pro.local>
2018-08-24 09:41:43 +1000

    Changed Distribution filenames

---
+++ Distribution/HappyBrackets Developer Kit/DeviceSetup/setup-pi-oracle.sh
@@ -0,0 +1,5 @@
+# Script to setup new pi disc image with HappyBrackets and appropriate details
+
+# run from pi, with an internet connection
+
+curl https://raw.githubusercontent.com/orsjb/HappyBrackets/master/DeviceSetup/setup-pi-image-oracle.sh | bash
---
+++ Distribution/HappyBrackets Developer Kit/DeviceSetup/setup-pi.sh
@@ -0,0 +1,6 @@
+# Script to setup new pi disc image with HappyBrackets and appropriate details
+
+# run from pi, with an internet connection
+#download image from Github
+
+curl https://raw.githubusercontent.com/orsjb/HappyBrackets/master/DeviceSetup/setup-pi-image.sh | bash
