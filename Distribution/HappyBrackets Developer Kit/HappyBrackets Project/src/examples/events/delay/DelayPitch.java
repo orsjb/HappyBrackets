7b73e42be6ceb27cde9bd65315b2a3b797bff99a
angelofraietta <angelo_f@smartcontroller.com.au>
2018-12-07 07:18:10 +1100

    Added extra examples

---
+++ Distribution/HappyBrackets Developer Kit/HappyBrackets Project/src/examples/events/delay/DelayPitch.java
@@ -0,0 +1,91 @@
+package examples.events.delay;
+
+import net.beadsproject.beads.data.Buffer;
+import net.happybrackets.core.HBAction;
+import net.happybrackets.core.HBReset;
+import net.happybrackets.core.instruments.WaveModule;
+import net.happybrackets.core.scheduling.Clock;
+import net.happybrackets.core.scheduling.Delay;
+import net.happybrackets.device.HB;
+
+import java.lang.invoke.MethodHandles;
+/*
+This sketch plays 400Khz Tone with a clock that changes its pitch to 1Khz for 300ms
+We will use a delay Object to triger a return to original pitch
+ */
+public class DelayPitch implements HBAction, HBReset {
+    // Change to the number of audio Channels on your device
+    final int NUMBER_AUDIO_CHANNELS = 1;
+
+    int ORIGINAL_PITCH = 400;
+    int HIGH_PITCH = 1000;
+    double HOLD_TIME = 300;
+
+
+    // This variable will become true when the composition is reset
+    boolean compositionReset = false;
+
+    @Override
+    public void action(HB hb) {
+        /***** Type your HBAction code below this line ******/
+        // remove this code if you do not want other compositions to run at the same time as this one
+        hb.reset();
+        hb.setStatus(this.getClass().getSimpleName() + " Loaded");
+
+        WaveModule waveModule = new WaveModule();
+        waveModule.setFequency(ORIGINAL_PITCH);
+        waveModule.setGain(0.1f);
+        waveModule.setBuffer(Buffer.SINE);
+        waveModule.connectTo(hb.ac.out);
+
+        /* To create this, just type clockTimer */
+        Clock clock = hb.createClock(500).addClockTickListener((offset, this_clock) -> {/* Write your code below this line */
+            waveModule.setFequency(HIGH_PITCH);
+
+            // now create the delay to switch back to original pitch
+            new Delay(HOLD_TIME, waveModule, (v, o) -> {
+                // v is how far out we were from our exact delay time in ms and is a double
+                // o is the parameter we passed in, which was the waveplayer
+                System.out.println("Delay offset by " + v + " ms");
+                ((WaveModule)o).setFequency(ORIGINAL_PITCH);
+            });
+
+            /* Write your code above this line */
+        });
+
+        clock.start();/* End Clock Timer */
+        
+
+
+        /***** Type your HBAction code above this line ******/
+    }
+
+
+    /**
+     * Add any code you need to have occur when a reset occurs
+     */
+    @Override
+    public void doReset() {
+        compositionReset = true;
+        /***** Type your HBReset code below this line ******/
+
+        /***** Type your HBReset code above this line ******/
+    }
+
+    //<editor-fold defaultstate="collapsed" desc="Debug Start">
+
+    /**
+     * This function is used when running sketch in IntelliJ IDE for debugging or testing
+     *
+     * @param args standard args required
+     */
+    public static void main(String[] args) {
+
+        try {
+            HB.runDebug(MethodHandles.lookup().lookupClass());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    //</editor-fold>
+}
---
+++ Distribution/HappyBrackets Developer Kit/HappyBrackets Project/src/examples/gpio/PulseDigitalOutGPIO.java
@@ -0,0 +1,125 @@
+package examples.gpio;
+
+import net.happybrackets.core.HBAction;
+import net.happybrackets.core.HBReset;
+import net.happybrackets.core.scheduling.Delay;
+import net.happybrackets.device.HB;
+import net.happybrackets.device.sensors.gpio.GPIO;
+import net.happybrackets.device.sensors.gpio.GPIODigitalOutput;
+
+import java.lang.invoke.MethodHandles;
+
+/**
+ * This composition will Pulse a digital output on GPIO_23 (pin 33 on PI header)
+ * It will pulse on for 500 Ms every 2 seconds
+ * See http://pi4j.com/pins/model-zero-rev1.html for pinouts
+ *
+ * connect cathode of LED through a resistance to earth and then connect anode to GPIO 23 output
+ *
+ * The state will be displayed in HB Status
+ *
+ *                                 ↗ ↗
+ *
+ *                                ┃ ╱┃
+ *    _________________╱╲  ╱╲  ___┃╱ ┃_______________ GPIO_23 (pin 33)
+ * __|__                 ╲╱  ╲╱   ┃╲ ┃
+ *  ___                           ┃ ╲┃
+ *   _
+ *
+ *******************************************************/
+public class PulseDigitalOutGPIO implements HBAction, HBReset {
+
+    boolean exitThread = false;
+
+    final int GPIO_OUTPUT = 23;
+
+    @Override
+    public void action(HB hb) {
+        /***** Type your HBAction code below this line ******/
+        // remove this code if you do not want other compositions to run at the same time as this one
+        hb.reset();
+        hb.setStatus(this.getClass().getSimpleName() + " Loaded");
+
+        // Reset all our GPIO - Only really necessary if the Pin has been assigned as something other than an input before
+        GPIO.resetAllGPIO();
+
+        /* Type gpioDigitalOut to create this code */
+        GPIODigitalOutput outputPin = GPIODigitalOutput.getOutputPin(GPIO_OUTPUT);
+        if (outputPin == null) {
+            hb.setStatus("Fail GPIO Digital Out " + GPIO_OUTPUT);
+        }/* End gpioDigitalOut code */
+
+
+        // Create a runnable thread to turn Output on and off
+        if (outputPin != null)
+        {
+            /* Type threadFunction to generate this code */
+            Thread thread = new Thread(() -> {
+                int SLEEP_TIME = 2000;
+                while (!exitThread) {/* write your code below this line */
+                    outputPin.setState(true);
+
+                    // now we will turn it off after 500 ms
+                    new Delay(500, outputPin, (v, o) -> {
+                        // v is how far out we were from our exact delay time in ms and is a double
+                        // o is the parameter we passed in, which was the output pin
+                        ((GPIODigitalOutput)o).setState(false);
+                    });
+                    /* write your code above this line */
+
+                    try {
+                        Thread.sleep(SLEEP_TIME);
+                    } catch (InterruptedException e) {
+                        /*** remove the break below to just resume thread or add your own action***/
+                        break;
+                        /*** remove the break above to just resume thread or add your own action ***/
+
+                    }
+                }
+
+                // we have left the loop turn off
+                outputPin.setState(false);
+            });
+
+            /*** write your code you want to execute before you start the thread below this line ***/
+
+            /*** write your code you want to execute before you start the thread above this line ***/
+
+            thread.start();
+            /****************** End threadFunction **************************/
+        }
+        /***** Type your HBAction code above this line ******/
+    }
+
+
+    /**
+     * Add any code you need to have occur when a reset occurs
+     */
+    @Override
+    public void doReset() {
+        /***** Type your HBReset code below this line ******/
+        // tell our thread to exit
+        exitThread = true;
+
+        // now disable our GPIO pin
+        GPIO.clearPinAssignment(GPIO_OUTPUT);
+        /***** Type your HBReset code above this line ******/
+    }
+
+    //<editor-fold defaultstate="collapsed" desc="Debug Start">
+
+    /**
+     * This function is used when running sketch in IntelliJ IDE for debugging or testing
+     *
+     * @param args standard args required
+     */
+    public static void main(String[] args) {
+
+        try {
+            HB.runDebug(MethodHandles.lookup().lookupClass());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    //</editor-fold>
+}
