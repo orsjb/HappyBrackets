4fbe79422a650c728921f8ff4a552eedf660c4d7
angelofraietta <angelo_f@smartcontroller.com.au>
2019-02-06 12:32:24 +1100

    3.5.1.0 Added send-folders Disabled automatic renaming of pi from script

--- HappyBrackets/src/main/java/net/happybrackets/core/BuildVersion.java
+++ HappyBrackets/src/main/java/net/happybrackets/core/BuildVersion.java
@@ -9,7 +9,7 @@ import java.net.URL;
 public final class BuildVersion {
     static final int MAJOR = 3;
     static final int MINOR = 5;
-    static final int BUILD = 0;
+    static final int BUILD = 1;
 
     public static int getMajor(){return MAJOR;}
     public static int getMinor() {return MINOR;}
