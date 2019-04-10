bc4cf5995d010cc3c055e328ca777312eea429ad
angelofraietta <angelo_f@smartcontroller.com.au>
2019-04-10 12:13:12 +1000

    made script run as sudo

--- DeviceSetup/setup-pi-image.sh
+++ DeviceSetup/setup-pi-image.sh
@@ -4,118 +4,119 @@
 
 # we must download and unzip HappyBrackets before we go into our sudo script so files will be unzipped to correct path
 
-cd
-# get the happybrackets zipped project folder
-# get latest filename
-FILENAME=$(curl http://www.happybrackets.net/downloads/happy-brackets-runtime.php?version)
+if [ "$(id -u)" != "0" ]; then
+    cd
+    # get the happybrackets zipped project folder
+    # get latest filename
+    FILENAME=$(curl http://www.happybrackets.net/downloads/happy-brackets-runtime.php?version)
 
-echo $FILENAME
-curl -O http://www.happybrackets.net/downloads/$FILENAME
-unzip $FILENAME
-rm $FILENAME
+    echo $FILENAME
+    curl -O http://www.happybrackets.net/downloads/$FILENAME
+    unzip $FILENAME
+    rm $FILENAME
+    // do everything else as root
+    exec sudo "$0" "$@"
+    exit 1
+else
 
-#everything below this needs to be sudo
-sudo -s <<SUDO_EXE
-
-# keep apt-get up to date with mirrors
-apt-get -y update
-
-
-#we need to install dirmngr so we can install zulu
-# https://blog.sleeplessbeastie.eu/2017/11/02/how-to-fix-missing-dirmngr/
-apt-get install dirmngr --install-recommends
-
-# install zeroconf
-apt-get -y --force-yes install libnss-mdns
-apt-get -y --force-yes install netatalk
-
-# install i2c tools
-apt-get -y --force-yes install i2c-tools
-
-#we need to installk wiringPI becasue it is not in STretch Lite
-sudo apt-get -y --force-yes install wiringpi
-
-# install ZULU java 8
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0x219BD9C9
-echo 'deb http://repos.azulsystems.com/debian stable main' > /etc/apt/sources.list.d/zulu.list
-
-apt-get update -qq
-apt-get install zulu-embedded-8
-
-#install Soft MIDI
-apt-get -y --force-yes install amsynth
-
-# Enable I2C on raspi, to connect to sensors. 
-# Counter-intuitively 'do_i2c 0' means 'enable'. 
-raspi-config nonint do_i2c 0
+    # keep apt-get up to date with mirrors
+    apt-get -y update
 
 
-# TODO setup audio if necessary.
-# set audio output to max volume, well not quite max but close
-amixer cset numid=1 0
-# save audio settings
-alsactl store
+    #we need to install dirmngr so we can install zulu
+    # https://blog.sleeplessbeastie.eu/2017/11/02/how-to-fix-missing-dirmngr/
+    apt-get install dirmngr --install-recommends
+
+    # install zeroconf
+    apt-get -y --force-yes install libnss-mdns
+    apt-get -y --force-yes install netatalk
+
+    # install i2c tools
+    apt-get -y --force-yes install i2c-tools
+
+    #we need to installk wiringPI becasue it is not in STretch Lite
+    sudo apt-get -y --force-yes install wiringpi
+
+    # install ZULU java 8
+    apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0x219BD9C9
+    echo 'deb http://repos.azulsystems.com/debian stable main' > /etc/apt/sources.list.d/zulu.list
+
+    apt-get update -qq
+    apt-get install zulu-embedded-8
+
+    #install Soft MIDI
+    apt-get -y --force-yes install amsynth
+
+    # Enable I2C on raspi, to connect to sensors.
+    # Counter-intuitively 'do_i2c 0' means 'enable'.
+    raspi-config nonint do_i2c 0
 
 
-# set up ssh login
-update-rc.d ssh enable
-invoke-rc.d ssh start
-
-#Add text to make it easy for people to change to HifiBerry
-#HIFI_TEXT=#dtoverlay=hifiberry-dac"
-#echo $HIFI_TEXT >> "/boot/config.txt"
+    # TODO setup audio if necessary.
+    # set audio output to max volume, well not quite max but close
+    amixer cset numid=1 0
+    # save audio settings
+    alsactl store
 
 
+    # set up ssh login
+    update-rc.d ssh enable
+    invoke-rc.d ssh start
+
+    #Add text to make it easy for people to change to HifiBerry
+    #HIFI_TEXT=#dtoverlay=hifiberry-dac"
+    #echo $HIFI_TEXT >> "/boot/config.txt"
 
 
-# now we need to see if we are going to start HappyBrackets from rc.local or from GUI Startup
-#we should be back to PI user
 
-GUI_FILE=/etc/xdg/lxsession/LXDE-pi/autostart
 
-if [ -f "$GUI_FILE" ]; then
-    echo "This PI is a GUI Program. We need to append our startup script to it"
-    # first let us add screen saver so we can disable it easy
-    apt-get install xscreensaver
+    # now we need to see if we are going to start HappyBrackets from rc.local or from GUI Startup
+    #we should be back to PI user
 
-    #now append our startup text
-    START_TEXT="@/usr/bin/sudo /home/pi/HappyBrackets/scripts/run.sh"
+    GUI_FILE=/etc/xdg/lxsession/LXDE-pi/autostart
 
-    #we need to add this startup text to GUI init file
-    #first see if it exists
-    if grep -Fxq "$START_TEXT" "$GUI_FILE"
-        then
-        # code if found
-        echo "Startup text already in GUI file";
-    else
-        # code if not found
-        echo "Append startup text to file"
-        echo $START_TEXT>>$GUI_FILE
+    if [ -f "$GUI_FILE" ]; then
+        echo "This PI is a GUI Program. We need to append our startup script to it"
+        # first let us add screen saver so we can disable it easy
+        apt-get install xscreensaver
+
+        #now append our startup text
+        START_TEXT="@/usr/bin/sudo /home/pi/HappyBrackets/scripts/run.sh"
+
+        #we need to add this startup text to GUI init file
+        #first see if it exists
+        if grep -Fxq "$START_TEXT" "$GUI_FILE"
+            then
+            # code if found
+            echo "Startup text already in GUI file";
+        else
+            # code if not found
+            echo "Append startup text to file"
+            echo $START_TEXT>>$GUI_FILE
+        fi
+
+    else # this is a standard non-gui PI
+        echo "This is a non gui PI. Add line to /etc/rc.local"
+        START_TEXT="/usr/bin/sudo /home/pi/HappyBrackets/scripts/run.sh"
+        RC_FILE=/etc/rc.local
+
+        #we need to add this startup text to GUI init file
+        #first see if it exists
+        if grep -Fxq "$START_TEXT" $RC_FILE
+            then
+            # code if found
+            echo "Startup text already in RC file";
+        else
+            # code if not found
+            # set up autorun
+            wget --no-check-certificate -N https://raw.githubusercontent.com/orsjb/HappyBrackets/master/DeviceSetup/rc.local
+            mv rc.local /etc/
+            chmod +x /etc/rc.local
+
+        fi
     fi
-
-else # this is a standard non-gui PI
-    echo "This is a non gui PI. Add line to /etc/rc.local"
-    START_TEXT="/usr/bin/sudo /home/pi/HappyBrackets/scripts/run.sh"
-    RC_FILE=/etc/rc.local
-
-    #we need to add this startup text to GUI init file
-    #first see if it exists
-    if grep -Fxq "$START_TEXT" $RC_FILE
-        then
-        # code if found
-        echo "Startup text already in RC file";
-    else
-        # code if not found
-        # set up autorun
-        wget --no-check-certificate -N https://raw.githubusercontent.com/orsjb/HappyBrackets/master/DeviceSetup/rc.local
-        mv rc.local /etc/
-        chmod +x /etc/rc.local
-
-    fi
+# end sudo
 fi
-
-#end sudo privilege here
-SUDO_EXE
 
 # Network Settings
 echo "***********************************"
